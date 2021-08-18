2ad9e9978282faae28205fcc95ca1065fcd50e62
JuanValdez <juantvaldez85@gmail.com>
2021-08-18 17:37:01 -0400

    Input module

---
+++ input.js
@@ -0,0 +1,21 @@
+const handleUserInput = function (key) {
+  process.stdin.on("data", (key) => {
+    process.stdout.write(".");
+  });
+
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+};
+
+// module.exports = setupInput;
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,25 +1,12 @@
-const net = require("net");
+// const net = require("net");
 const connect = require("./client");
-
+const setupInput = require("./input");
 /**
  * Establishes connection with the game server
  */
-const handleUserInput = function () {
-  process.stdin.on("data", (key) => {
-    process.stdout.write(".");
-  });
-};
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
+// handleUserInput();
 
-  return stdin;
-};
-
-handleUserInput();
-setupInput();
 console.log("Connecting ...");
 connect();
+setupInput();
